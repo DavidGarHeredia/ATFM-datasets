1bd96d7043c1aed76e6894c077dd3c3dad3ee3a0
DavidGarHeredia <dgheredia@protonmail.com>
2021-07-10 18:44:24 +0200

    test for filter_observation_and_...

--- tests/test_functions_step1.jl
+++ tests/test_functions_step1.jl
@@ -6,11 +6,20 @@ include(testdir*"/../functions_step1.jl")
 
 
 @testset "remove_flights_in_non_contiguous_USA" begin
-	println()
 	df = DataFrame(load(testdir*"/fakeFlightsForTests.csv"))
 	territoriesToDelte = ["Alaska", "Hawaii"];
 	@test nrow(df) == 6
 	df_contiguous = remove_flights_in_non_contiguous_USA!(territoriesToDelte, df)
 	@test nrow(df) == 6
 	@test nrow(df_contiguous) == 3
+end
+
+@testset "filter_observation_and_keep_only_useful_columns" begin
+	df = DataFrame(load(testdir*"/fakeFlightsForTests.csv"))
+	@test nrow(df) == 6
+	df_filtered = filter_observation_and_keep_only_useful_columns(df, 5)
+	@test nrow(df) == 6
+	@test nrow(df_filtered) == 2
+	@test df_filtered[1, :Tail_Number] == "tail1"
+	@test df_filtered[2, :Tail_Number] == "tail4"
 end
