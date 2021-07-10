b6446112dd8330f60e76cda71187f3e28dc5c00d
DavidGarHeredia <dgheredia@protonmail.com>
2021-07-10 19:35:16 +0200

    tests for clean_data_flights

--- tests/test_functions_step1.jl
+++ tests/test_functions_step1.jl
@@ -1,33 +1,38 @@
+using Base: close_chnl_on_taskdone
 
 using Test
 using Queryverse
 const testdir = dirname(@__FILE__)
 include(testdir*"/../functions_step1.jl")
 
+df_flights = DataFrame(load(testdir*"/fakeFlightsForTests.csv"))
+df_airports = DataFrame(load(testdir*"/fakeAirportsForTests.csv"))
+territoriesToDelte = ["Alaska", "Hawaii"];
+@test nrow(df_flights) == 7
 
 @testset "remove_flights_in_non_contiguous_USA" begin
-	df = DataFrame(load(testdir*"/fakeFlightsForTests.csv"))
-	territoriesToDelte = ["Alaska", "Hawaii"];
-	@test nrow(df) == 7
-	df_contiguous = remove_flights_in_non_contiguous_USA!(territoriesToDelte, df)
-	@test nrow(df) == 7
+	df_contiguous = remove_flights_in_non_contiguous_USA!(territoriesToDelte, df_flights)
+	@test nrow(df_flights) == 7
 	@test nrow(df_contiguous) == 4
+	@test df_contiguous[1, :OriginStateName] == "California"
+	@test df_contiguous[1, :DestStateName] == "California"
+	@test df_contiguous[2, :OriginStateName] == "Fake"
+	@test df_contiguous[2, :DestStateName] == "Fake"
+	@test df_contiguous[3, :OriginStateName] == "Fake"
+	@test df_contiguous[3, :DestStateName] == "Fake"
+	@test df_contiguous[4, :OriginStateName] == "California"
+	@test df_contiguous[4, :DestStateName] == "Idaho"
 end
 
 @testset "filter_observation_and_keep_only_useful_columns" begin
-	df = DataFrame(load(testdir*"/fakeFlightsForTests.csv"))
-	@test nrow(df) == 7
-	df_filtered = filter_observation_and_keep_only_useful_columns(df, 5)
-	@test nrow(df) == 7
+	df_filtered = filter_observation_and_keep_only_useful_columns(df_flights, 5)
+	@test nrow(df_flights) == 7
 	@test nrow(df_filtered) == 2
 	@test df_filtered[1, :Tail_Number] == "tail1"
 	@test df_filtered[2, :Tail_Number] == "tail4"
 end
 
 @testset "remove_flights_with_no_airport_in_the_data_set" begin
-	df_flights = DataFrame(load(testdir*"/fakeFlightsForTests.csv"))
-	df_airports = DataFrame(load(testdir*"/fakeAirportsForTests.csv"))
-	@test nrow(df_flights) == 7
 	df_removed = remove_flights_with_no_airport_in_the_data_set(df_airports, df_flights)
 	@test nrow(df_flights) == 7
 	@test nrow(df_removed) == 2
@@ -35,4 +40,16 @@ end
 	@test df_removed[1, :DestAirportID] == 14771
 	@test df_removed[2, :OriginAirportID] == 14771 
 	@test df_removed[2, :DestAirportID] == 10713
+end
+
+@testset "clean_data_flights" begin
+	df_final = clean_data_flights(df_flights, 5, territoriesToDelte, df_airports)
+	@test nrow(df_flights) == 7
+	@test nrow(df_final) == 2
+	@test df_final[1, :OriginAirportID] == 14698
+	@test df_final[1, :DestAirportID] == 14771
+	@test df_final[2, :OriginAirportID] == 14771 
+	@test df_final[2, :DestAirportID] == 10713
+	@test df_final[1, :Tail_Number] == "tail1"
+	@test df_final[2, :Tail_Number] == "tail4"
 end
